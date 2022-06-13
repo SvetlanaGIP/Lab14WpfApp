764d3e0c811e1aac62e5f23e342c316d6521f730
SvetlanaGIP <gip1@acn.su>
2022-06-13 11:08:00 +1000

    Добавьте файлы проекта.

---
+++ Lab14WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab14WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Problem>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            problems = new List<Problem>();
+            problems.Add(new Problem()
+            {
+                ProblemName = "Task1",
+                Description = "To Do Homework",
+                Priority = 2,
+                ProblemType = ProblemTypes.Work
+            });
+            problems.Add(new Problem()
+            {
+                ProblemName = "Task2",
+                Description = "To water the flowers",
+                Priority = 1,
+                ProblemType = ProblemTypes.Home
+            });
+            problems.Add(new Problem()
+            {
+                ProblemName = "Task3",
+                Description = "To make dinner",
+                Priority = 1,
+                ProblemType = ProblemTypes.Work
+            });
+            lstBx.ItemsSource = problems;
+        }
+    }
+}
---
+++ Lab14WpfApp/Problem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14WpfApp
+{
+    public enum ProblemTypes
+    {
+        Home,
+        Work
+    }
+    public class Problem
+    {
+        public string ProblemName { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public ProblemTypes ProblemType { get; set; }
+    }
+}
